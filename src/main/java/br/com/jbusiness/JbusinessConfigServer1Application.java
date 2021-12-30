fd017acde931c5c091e26406f8e16fc4522d999a
rodrigoks <rodrigoks@gmail.com>
2021-12-30 19:02:10 -0300

    Adicionados codigos iniciais.

---
+++ src/main/java/br/com/jbusiness/JbusinessConfigServer1Application.java
@@ -0,0 +1,13 @@
+package br.com.jbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbusinessConfigServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbusinessConfigServer1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jbusiness/JbusinessConfigServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbusinessConfigServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

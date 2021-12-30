c65ad4125d55d6236998965e730413a5d34d03ee
rodrigoks <rodrigoks@gmail.com>
2021-12-30 19:03:48 -0300

    renomeando classes.

--- src/main/java/br/com/jbusiness/JbusinessConfigServerApplication.java
+++ src/main/java/br/com/jbusiness/JbusinessConfigServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JbusinessConfigServer1Application {
+public class JbusinessConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JbusinessConfigServer1Application.class, args);
+		SpringApplication.run(JbusinessConfigServerApplication.class, args);
 	}
 
 }
